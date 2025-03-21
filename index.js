83e140784f8aa07b793691f63a8560f02a65413b
imyooni <120503455+imyooni@users.noreply.github.com>
2025-03-21 15:39:01 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+let rooms = {}; // Store active rooms
+
+io.on("connection", (socket) => {
+    console.log(`Player connected: ${socket.id}`);
+
+    socket.on("createRoom", (roomCode) => {
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = { players: [] };
+        }
+
+        if (rooms[roomCode].players.length < 2) {
+            rooms[roomCode].players.push(socket.id);
+            socket.join(roomCode);
+            console.log(`Player ${socket.id} joined room ${roomCode}`);
+
+            io.to(roomCode).emit("roomUpdate", rooms[roomCode].players);
+
+            if (rooms[roomCode].players.length === 2) {
+                io.to(roomCode).emit("gameStart", "Game is starting!");
+            }
+        } else {
+            socket.emit("roomFull", "This room is already full!");
+        }
+    });
+
+    socket.on("playerMove", (data) => {
+        io.to(data.roomCode).emit("updateGame", data.position);
+    });
+
+    socket.on("disconnect", () => {
+        for (const roomCode in rooms) {
+            rooms[roomCode].players = rooms[roomCode].players.filter(id => id !== socket.id);
+            if (rooms[roomCode].players.length === 0) {
+                delete rooms[roomCode]; // Remove empty rooms
+            } else {
+                io.to(roomCode).emit("roomUpdate", rooms[roomCode].players);
+            }
+        }
+        console.log(`Player disconnected: ${socket.id}`);
+    });
+});
+
+server.listen(4000, () => console.log("Server running on port 4000"));
